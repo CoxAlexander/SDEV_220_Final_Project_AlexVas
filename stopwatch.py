bae976b51c1aa17bc812d6d4569baab01d89956f
CoxAlexander <coxalexander593@gmail.com>
2025-02-03 14:39:38 -0500

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -3,10 +3,12 @@ class stopWatch:
     def __init__(self, startTime):
         self.startTime = startTime
     def currentTime(self,currentTime):
-        timepassed = currentTime-self.startTime
+        timepassedsec = currentTime-self.startTime
+        timepassed = time.strftime("%H:%M:%S",time.gmtime(timepassedsec))
         return timepassed
 timer = stopWatch(startTime=time.time())
-time.sleep(10)
-print(timer.currentTime(currentTime=time.time()))
-
-    
+counter = 0
+while counter in range(0,100):
+    print(timer.currentTime(currentTime=time.time()))
+    time.sleep(1)
+    counter += 1
