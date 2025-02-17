0d3f5c29a4e20c90c94984c8573b882d3e0b8ef3
CoxAlexander <coxalexander593@gmail.com>
2025-02-17 14:06:26 -0500

    stopwatch gui

--- stopwatch.py
+++ stopwatch.py
@@ -1,14 +1,23 @@
 import time 
 class stopWatch:
     def __init__(self, startTime):
+        lap = 0
         self.startTime = startTime
     def currentTime(self,currentTime):
         timepassedsec = currentTime-self.startTime
         timepassed = time.strftime("%H:%M:%S",time.gmtime(timepassedsec))
+        print(timepassed)
         return timepassed
+    
+"""
 timer = stopWatch(startTime=time.time())
 counter = 0
 while counter in range(0,100):
     print(timer.currentTime(currentTime=time.time()))
     time.sleep(1)
     counter += 1
+"""
+    
+if __name__ == "__main__":
+    timer = stopWatch(startTime=time.time())
+    timer.currentTime(time.time())
---
+++ stopwatchGui.py
@@ -0,0 +1,40 @@
+import stopwatch 
+import tkinter as tk
+import time
+
+class gui:
+    stop = True
+    def __init__(self):
+        self.timer = stopwatch.stopWatch(startTime=time.time())
+        self.root = tk.Tk()
+        self.root.geometry('500x250')
+        self.root.title('Timer')
+        self.time_label = tk.Label(self.root, font = ('Helvetica', 30), text = self.timer.currentTime(time.time()))
+        self.time_label.grid(row = 0, column = 0, columnspan = 5, padx = 5, pady = 5)        
+        self.start_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'Begin', command = self.start)
+        self.start_button.grid(row = 1, column = 0, columnspan = 2, padx = 5, pady = 5)
+        self.stop_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'End', command = self.stop)
+        self.stop_button.grid(row = 1, column = 2, columnspan = 2, padx = 5, pady = 5)
+        self.root.mainloop()
+    
+    
+    def countup(self):
+        while not(gui.stop):
+           self.timer.currentTime(time.time())
+           self.time_label.config(text=self.timer.currentTime(time.time()))
+           self.root.update()
+    
+           
+    def start(self):
+        gui.stop = False
+        self.countup()
+    
+        
+    def stop(self):
+        gui.stop = True
+        self.time_label.config(text="00:00:00")
+        self.countup()
+    
+        
+if __name__ == "__main__":
+    gui()
