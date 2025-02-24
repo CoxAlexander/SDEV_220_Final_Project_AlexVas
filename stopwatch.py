4668f3995f5efc5be7317878feec25a535c5460d
CoxAlexander <coxalexander593@gmail.com>
2025-02-24 17:24:50 -0500

    added pause

--- stopwatch.py
+++ stopwatch.py
@@ -1,15 +1,26 @@
 import time 
 class stopWatch:
     def __init__(self, startTime):
-        lap = 0
+        self.lap = 0
+        self.elaspedTime = 0
         self.startTime = startTime
+        
     def currentTime(self,currentTime):
         timepassedsec = currentTime-self.startTime
-        timepassed = time.strftime("%H:%M:%S",time.gmtime(timepassedsec))
-        print(timepassed)
-        return timepassed
+        self.elaspedTime += timepassedsec
+        elaspedTimeFormat = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
+        return elaspedTimeFormat
+    
+    def pauseTime(self,currentTime):
+        timepassedsec = currentTime - self.startTime
+        self.elaspedTime += timepassedsec
+        elaspedTimeFormat = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
+        self.startTime = currentTime
+        return(elaspedTimeFormat)
+
     
     
 if __name__ == "__main__":
     timer = stopWatch(startTime=time.time())
-    timer.currentTime(time.time())
+    time.sleep(10)
+    print(timer.currentTime(time.time()))
--- stopwatchGui.py
+++ stopwatchGui.py
@@ -4,6 +4,8 @@ import time
 
 class gui:
     stop = True
+    paused = False
+    pause_count = 0
     def __init__(self):
         self.timer = sw.stopWatch(startTime=time.time())
         self.root = tk.Tk()
@@ -12,30 +14,45 @@ class gui:
         self.time_label = tk.Label(self.root, font = ('Helvetica', 30), text = self.timer.currentTime(time.time()))
         self.time_label.grid(row = 0, column = 0, columnspan = 5, padx = 5, pady = 5)        
         self.start_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'Begin', command = self.start)
-        self.start_button.grid(row = 1, column = 0, columnspan = 2, padx = 5, pady = 5)
+        self.start_button.grid(row = 1, column = 0, padx = 10, pady = 5)
         self.stop_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'End', command = self.stop)
-        self.stop_button.grid(row = 1, column = 2, columnspan = 2, padx = 5, pady = 5)
+        self.stop_button.grid(row = 1, column = 1,  padx = 10, pady = 5)
+        self.pause_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'Pause', command = self.pause)
+        self.pause_button.grid(row = 1, column = 2,  padx = 10, pady = 5)
         self.root.mainloop()
     
     
     def countup(self):
-        while not(gui.stop):
-           self.timer.currentTime(time.time())
+        while not(gui.stop and gui.pause):
            self.time_label.config(text=self.timer.currentTime(time.time()))
+           self.timer.currentTime(time.time())
            self.root.update()
-    
+           
+    def pause(self):
+        self.pause_count += 1
+        if  (gui.pause):
+            self.timer.pauseTime(time.time())
+            self.time_label.config(text=self.timer.pauseTime(time.time()))
+            self.pause_button.config(text="Pause")
+            gui.pause = False
+        else:
+            self.pause_button.config(text="Unpause")
+            gui.pause = True
+            gui.stop = True
+        self.countup()
            
     def start(self):
         gui.stop = False
+        gui.pause = False
         self.countup()
-    
         
     def stop(self):
         gui.stop = True
+        gui.pause = True
+        self.pause_button.config(text="Pause")
         self.time_label.config(text="00:00:00")
         self.timer = sw.stopWatch(startTime=time.time())
         self.countup()
-    
         
 if __name__ == "__main__":
     gui()
