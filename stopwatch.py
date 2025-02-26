46b55ae6d1eb9135c26d10fd786b36343ee8ae1f
CoxAlexander <coxalexander593@gmail.com>
2025-02-26 02:42:24 -0500

    Pausing now works

--- stopwatch.py
+++ stopwatch.py
@@ -1,26 +1,40 @@
 import time 
 class stopWatch:
     def __init__(self, startTime):
-        self.lap = 0
-        self.elaspedTime = 0
-        self.startTime = startTime
+        self.lapNum: int = 0
+        self.lapTime: int = 0
+        self.lap: dict = {self.lapNum : self.lapTime}
+        self.elaspedTime: float = 0
+        self.startTime: float = startTime
+        self.passedTime: float = 0
         
-    def currentTime(self,currentTime):
-        timepassedsec = currentTime-self.startTime
-        self.elaspedTime += timepassedsec
-        elaspedTimeFormat = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
-        return elaspedTimeFormat
+        
+    def currentTime(self,currentTime: float):
+        self.elaspedTime =  ((currentTime - self.startTime) - self.passedTime)
+        self.elaspedTimeFormat: tuple = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
+        return (self.elaspedTimeFormat)
     
-    def pauseTime(self,currentTime):
-        timepassedsec = currentTime - self.startTime
-        self.elaspedTime += timepassedsec
-        elaspedTimeFormat = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
-        self.startTime = currentTime
-        return(elaspedTimeFormat)
+    
+    def pauseTime(self, pausedTime:float):
+        self.passedTime += pausedTime
+        return(self.elaspedTimeFormat)
 
-    
-    
+
+    def pausedTime(self,currentTime:float):
+        pausedTime = currentTime - self.startTime
+        return pausedTime
+        
+
+    def lap(self):
+        self.lapNum += 1
+        self.lapTime = self.currentTime()
+        self.lap[self.lapNum] = self.lapTime
+        
+        
 if __name__ == "__main__":
     timer = stopWatch(startTime=time.time())
-    time.sleep(10)
-    print(timer.currentTime(time.time()))
+    counter = 0
+    while counter <= 90:
+        print(timer.currentTime(time.time()))
+        counter += 1
+        time.sleep(1)
--- stopwatchGui.py
+++ stopwatchGui.py
@@ -5,13 +5,13 @@ import time
 class gui:
     stop = True
     paused = False
-    pause_count = 0
     def __init__(self):
-        self.timer = sw.stopWatch(startTime=time.time())
+        self.timer = sw.stopWatch(startTime = time.time())
+        self.pausedTimer = sw.stopWatch(startTime=time.time())
         self.root = tk.Tk()
         self.root.geometry('500x250')
-        self.root.title('Timer')
-        self.time_label = tk.Label(self.root, font = ('Helvetica', 30), text = self.timer.currentTime(time.time()))
+        self.root.title('StopWatch')
+        self.time_label = tk.Label(self.root, font = ('Helvetica', 30), text =  "00:00:00") #self.timer.currentTime(time.time()))
         self.time_label.grid(row = 0, column = 0, columnspan = 5, padx = 5, pady = 5)        
         self.start_button = tk.Button(self.root, font = ('Helvetica', 30), text = 'Begin', command = self.start)
         self.start_button.grid(row = 1, column = 0, padx = 10, pady = 5)
@@ -24,35 +24,43 @@ class gui:
     
     def countup(self):
         while not(gui.stop and gui.pause):
-           self.time_label.config(text=self.timer.currentTime(time.time()))
            self.timer.currentTime(time.time())
+           self.time_label.config(text = self.timer.currentTime(time.time()))
            self.root.update()
            
+           
     def pause(self):
-        self.pause_count += 1
-        if  (gui.pause):
-            self.timer.pauseTime(time.time())
-            self.time_label.config(text=self.timer.pauseTime(time.time()))
-            self.pause_button.config(text="Pause")
+        
+        if(gui.pause):
+            gui.stop = False
             gui.pause = False
+            self.pausedTimer.pausedTime(currentTime=time.time())
+            self.timer.pauseTime(pausedTime=self.pausedTimer.pausedTime(currentTime=time.time()))
+            self.pause_button.config(text="Pause")
+            
+            
         else:
+            self.pausedTimer = sw.stopWatch(startTime=time.time())
             self.pause_button.config(text="Unpause")
             gui.pause = True
             gui.stop = True
         self.countup()
-           
+        
     def start(self):
         gui.stop = False
         gui.pause = False
+        self.timer = sw.stopWatch(startTime = time.time())
         self.countup()
+        
         
     def stop(self):
         gui.stop = True
         gui.pause = True
-        self.pause_button.config(text="Pause")
-        self.time_label.config(text="00:00:00")
+        self.pause_button.config(text = "Pause")
+        self.time_label.config(text = "00:00:00")
         self.timer = sw.stopWatch(startTime=time.time())
-        self.countup()
+        
+        
         
 if __name__ == "__main__":
     gui()
