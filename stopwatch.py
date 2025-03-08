dc398ddd42e179c1d27bd43b61ab6c6f3dc0d7cc
CoxAlexander <coxalexander593@gmail.com>
2025-03-07 23:51:11 -0500

    saving to database works

--- stopwatch.py
+++ stopwatch.py
@@ -1,5 +1,5 @@
 import time 
-import sqlalchemy as sa 
+import sqlalchemy as sa
 class stopWatch:
     
     
@@ -7,18 +7,20 @@ class stopWatch:
         self.engine = sa.create_engine('sqlite:///time.db')
         self.conn = self.engine.connect()
         self.meta = sa.MetaData() 
-    
         self.watch_table = sa.Table('watch', self.meta,
                      sa.Column('row', sa.Integer,primary_key=True),
                      sa.Column('passedTime',sa.REAL),
                      sa.Column('startTime',sa.REAL)
                     )
-
         self.elaspedTime: float = 0
         self.startTime: float = startTime
         self.passedTime: float = 0 
         
+    def closeDatabase(self):
+        self.conn.close()
+        self.engine.dispose()
         
+            
     def currentTime(self,currentTime: float):
         self.elaspedTime =  ((currentTime - self.startTime) - self.passedTime)
         self.elaspedTimeFormat: tuple = time.strftime("%H:%M:%S",time.gmtime(self.elaspedTime))
@@ -48,14 +50,37 @@ class stopWatch:
 
 
     def storeData(self,pT,sT):
-       self.conn.execute(self.watch_table.update().where(self.watch_table.c.row == 1).values(passedTime = pT, startTime=sT))
-
+        self.conn.execute(self.watch_table.update().where(self.watch_table.c.row == 1).values(passedTime = pT, startTime=sT))
+        self.conn.commit()
+        
+        
 class lap(stopWatch):
     def __init__(self, startTime):
         super().__init__(startTime)
+        self.engine = sa.create_engine('sqlite:///time.db')
+        self.conn = self.engine.connect()
+        self.meta = sa.MetaData() 
+        self.lap_table = sa.Table('lap', self.meta,
+                     sa.Column('lapNum', sa.Integer,primary_key=True),
+                     sa.Column('lapTime',sa.TEXT),
+                    )
+       
         self.lapNum: int = 0
         self.lapTime: int = 0
         self.lapList: list = []
+    
+    def dropLapTable(self):
+        sql = sa.delete(self.lap_table)
+        with self.engine.connect() as connection:
+            result = connection.execute(sql)
+            connection.commit()
+        
+    
+    def getLapNum(self):
+        return self.lapNum
+    
+    def getLapTime(self):
+        return self.lapTime
         
     def lapFunc(self,lapTime):
         self.lapNum += 1
@@ -67,8 +92,11 @@ class lap(stopWatch):
     def retrieveData(self):
         pass
 
-    def storeData(self):
-        pass
+    def storeLapData(self,lN,lT):
+        sql = sa.insert(self.lap_table).values(lapNum=lN, lapTime=lT)
+        with self.engine.connect() as connection:
+            result = connection.execute(sql)
+            connection.commit()
     
     
 if __name__ == "__main__":
@@ -76,7 +104,6 @@ if __name__ == "__main__":
     timer.meta.create_all(timer.engine)
     counter = 0
     while counter <= 10:
-        #1741239562.36468
         print(timer.currentTime(time.time()))
         counter += 1
         time.sleep(1)
--- stopwatchGui.py
+++ stopwatchGui.py
@@ -9,9 +9,10 @@ class gui:
         self.lapNum:int = 0
         self.height: int = 200 + (50*self.lapNum)
         self.length: int = 600
-        self.lapList = [] 
+        self.lapList = []
         self.timer = sw.stopWatch(startTime = time.time())
         self.pausedTimer = sw.stopWatch(startTime=time.time())
+        self.lapTimer = sw.lap(startTime=time.time())
         self.root = tk.Tk()
         self.screenWidth = self.root.winfo_screenwidth()
         self.screenHeight = self.root.winfo_screenheight()
@@ -33,65 +34,76 @@ class gui:
     
     
     def countup(self):
-        while not(gui.stop and gui.pause):
-           self.timer.currentTime(time.time())
-           self.time_label.config(text = self.timer.currentTime(time.time()))
-           self.root.update()
-           self.timer.storeData(pT=self.timer.getPassedTime(), sT=self.timer.getStartTime)
+        while not(gui.stop or gui.pause):
+            self.timer.currentTime(time.time())
+            self.time_label.config(text = self.timer.currentTime(time.time()))
+            self.root.update()
+        self.timer.storeData(pT=self.timer.getPassedTime(), sT=self.timer.getStartTime())
     
            
     def lap(self):
-        self.height: int = 300 + (50*self.lapNum)
-        self.length: int = 600 
-        self.lapTime = sw.lap(startTime=time.time())
-        self.lapNum +=1
-        self.lapList.append(tk.Label(self.root, font=('Helvetica' ,20), text=f'lap {self.lapNum}: {self.lapTime.lapFunc(self.timer.currentTime(time.time()))}'))
-        self.lapList[self.lapNum-1].grid(row=(self.lapNum),column = 0, columnspan = 5, padx = 10, pady = 5)
-        self.start_button.grid(row = (self.lapNum+1), column = 0, padx = 10, pady = 5)
-        self.stop_button.grid(row = (self.lapNum+1), column = 1,  padx = 10, pady = 5)
-        self.pause_button.grid(row = (self.lapNum+1), column = 2,  padx = 0, pady = 5)
-        self.lap_button.grid(row = (self.lapNum+1), column = 3,  padx = 10, pady = 5)
-        self.root.geometry(f'{self.length}x{self.height}')
-        self.root.update()
-    
+        if not(gui.stop or gui.pause):
+            self.height: int = 300 + (50*self.lapNum)
+            self.length: int = 600 
+            self.lapTimer = sw.lap(startTime=time.time())
+            self.lapNum +=1
+            self.lapList.append(tk.Label(self.root, font=('Helvetica' ,20), text=f'lap {self.lapNum}: {self.lapTimer.lapFunc(self.timer.currentTime(time.time()))}'))
+            self.lapList[self.lapNum-1].grid(row=(self.lapNum),column = 0, columnspan = 5, padx = 10, pady = 5)
+            self.start_button.grid(row = (self.lapNum+1), column = 0, padx = 10, pady = 5)
+            self.stop_button.grid(row = (self.lapNum+1), column = 1,  padx = 10, pady = 5)
+            self.pause_button.grid(row = (self.lapNum+1), column = 2,  padx = 0, pady = 5)
+            self.lap_button.grid(row = (self.lapNum+1), column = 3,  padx = 10, pady = 5)
+            self.root.geometry(f'{self.length}x{self.height}')
+            self.root.update()
+            self.timer.storeData(pT=self.timer.getPassedTime(), sT=self.timer.getStartTime())
+            self.lapTimer.storeLapData(lN=self.lapNum,lT=self.lapTimer.getLapTime())
+        
                
     def pause(self):
-        if(gui.pause):
-            gui.stop = False
-            gui.pause = False
-            self.pausedTimer.pausedTime(currentTime=time.time())
-            self.timer.pauseTime(pausedTime=self.pausedTimer.pausedTime(currentTime=time.time()))
-            self.pause_button.config(text="Pause")
-            
-        else:
-            self.pausedTimer = sw.stopWatch(startTime=time.time())
-            self.pause_button.config(text="Resume")
-            gui.pause = True
-            gui.stop = True
-        self.countup()
+        if not(gui.stop and gui.paused):
+            if(gui.pause):
+                
+                gui.pause = False
+                self.pausedTimer.pausedTime(currentTime=time.time())
+                self.timer.pauseTime(pausedTime=self.pausedTimer.pausedTime(currentTime=time.time()))
+                self.pause_button.config(text="Pause")
+                
+            else:
+                self.pausedTimer = sw.stopWatch(startTime=time.time())
+                self.pause_button.config(text="Resume")
+                gui.pause = True
+                
+            self.timer.storeData(pT=self.timer.getPassedTime(), sT=self.timer.getStartTime())
+            self.countup()
+        
         
     def start(self):
         gui.stop = False
         gui.pause = False
         self.timer = sw.stopWatch(startTime = time.time())
+        self.timer.storeData(0,0)
         self.countup()
         
         
-    def stop(self):
-        gui.stop = True
-        gui.pause = True
-        self.pause_button.config(text = "Pause")
-        self.time_label.config(text = "00:00:00")
-        for x in range(0,len(self.lapList)):
-            self.lapList[x].destroy()
-        self.lapNum = 0
-        self.lapList = [] 
-        self.timer = sw.stopWatch(startTime=time.time())
-        self.height: int = 200 + (50*self.lapNum)
-        self.length: int = 600
-        self.root.geometry(f'{self.length}x{self.height}')
-        self.root.update()
         
+    def stop(self):
+        if not gui.stop:
+            gui.stop = True
+            gui.pause = True
+            self.pause_button.config(text = "Pause")
+            self.time_label.config(text = "00:00:00")
+            for x in range(0,len(self.lapList)):
+                self.lapList[x].destroy()
+            self.lapNum = 0
+            self.lapList = [] 
+            self.timer = sw.stopWatch(startTime=time.time())
+            self.height: int = 200 + (50*self.lapNum)
+            self.length: int = 600
+            self.root.geometry(f'{self.length}x{self.height}')
+            self.root.update()
+            self.timer.storeData(0,0)
+            self.lapTimer.dropLapTable()
+            
         
         
 if __name__ == "__main__":
